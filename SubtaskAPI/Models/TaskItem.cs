2c26c42ff5d3315ec315a844d5da17aea825255a
Piotr Zysk <pizy123@gmail.com>
2019-07-22 12:09:59 +0200

    Add project files.

---
+++ SubtaskAPI/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubtaskAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id;
+        public int ParentId;
+        public string Title;
+        public bool Done;
+    }
+}
