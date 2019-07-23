2e12c5f71a9ee4730a27f62ca19e517e79576c6c
Piotr Zysk <pizy123@gmail.com>
2019-07-23 11:50:45 +0200

    DB source connected to Repo

--- SubtaskAPI/Repositories/TaskRepository.cs
+++ SubtaskAPI/Repositories/TaskRepository.cs
@@ -1,35 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
+using NHibernate;
 using SubtaskAPI.Models;
 
 namespace SubtaskAPI.Repositories
 {
     public class TaskRepository : ITaskRepository
     {
-        private List<TaskItem> _entities;
+        private readonly ISession _session;
 
-        private string _order;
-
-        public TaskRepository()
+        public TaskRepository(ISession session)
         {
-            this._entities = new List<TaskItem>();
-            this._entities.Add(new TaskItem { Done = true, Id = 1, ParentId = 0, Title = "blabla lala"} );
-            this._entities.Add(new TaskItem { Done = false, Id = 2, ParentId = 0, Title = "dubi dubi da" });
-            this._entities.Add(new TaskItem { Done = false, Id = 3, ParentId = 2, Title = "podtask dubi" });
-            this._entities.Add(new TaskItem { Done = false, Id = 4, ParentId = 2, Title = "podtask dubi 2" });
-            this._entities.Add(new TaskItem { Done = false, Id = 5, ParentId = 1, Title = "podtask bla" });
-            this._entities.Add(new TaskItem { Done = false, Id = 6, ParentId = 0, Title = "single element" });
-
-            this._order = "1,2,6";
-
+            this._session = session;
         }
 
+        //private List<TaskItem> _entities;
+
+        //private string _order;
+
+        //public TaskRepository()
+        //{
+        //    this._entities = new List<TaskItem>();
+        //    this._entities.Add(new TaskItem { Done = true, Id = 1, ParentId = 0, Title = "blabla lala"} );
+        //    this._entities.Add(new TaskItem { Done = false, Id = 2, ParentId = 0, Title = "dubi dubi da" });
+        //    this._entities.Add(new TaskItem { Done = false, Id = 3, ParentId = 2, Title = "podtask dubi" });
+        //    this._entities.Add(new TaskItem { Done = false, Id = 4, ParentId = 2, Title = "podtask dubi 2" });
+        //    this._entities.Add(new TaskItem { Done = false, Id = 5, ParentId = 1, Title = "podtask bla" });
+        //    this._entities.Add(new TaskItem { Done = false, Id = 6, ParentId = 0, Title = "single element" });
+
+        //    this._order = "1,2,6";
+
+        //}
+
         public IEnumerable<TaskItem> GetAllTaskItems()
-            => this._entities;
+            => this._session.Query<TaskItem>().ToList();
 
         public string GetIdsString()
-            => this._order;
+            => this._session.Query<TaskOrder>().Select(s => s.Ids).FirstOrDefault();
     }
 }
